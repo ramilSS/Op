11f7b0caadc6eb5b7f1add36b5d30a9020c6146d
ramilSS <127701391+ramilSS@users.noreply.github.com>
2023-06-10 08:54:55 +0300

    Update config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+close_options = ['Да', 'Нет']
+questions = [
+    {'text': 'Часто ли вы впадаете в гнев?', 'type': 'closed', 'options': close_options},
+    {'text': 'Боитесь ли вы человека в гневе, который физически крепче вас?', 'type': 'closed', 'options': close_options},
+    {'text': 'Что обратить на себя внимание, вы начинаете вредничать/скандалить', 'type': 'closed', 'options': close_options},
+    {'text': 'Любите ли вы гонять на автомобилях', 'type': 'closed', 'options': close_options},
+    {'text': 'Используете лекарства при болезни?', 'type': 'closed', 'options': close_options},
+    {'text': 'Сделаете ли вы всё, что достичь своей цели?', 'type': 'closed', 'options': close_options},
+    {'text': 'Любите ли вы кошек?', 'type': 'closed', 'options': close_options},
+    {'text': 'Любите ли вы сидеть часами гулять?', 'type': 'closed', 'options': close_options},
+    {'text': 'У вас есть мечта стать знаменитым?', 'type': 'closed', 'options': close_options},
+    {'text': 'Умеете ли вы вовремя остановиться?', 'type': 'closed', 'options': close_options},
+    {'text': 'Вы много едите?', 'type': 'closed', 'options': close_options},
+    {'text': 'Любите ли вы играть в игры?', 'type': 'closed', 'options': close_options}
+]
